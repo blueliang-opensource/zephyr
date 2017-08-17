ee4bd0dc11f0710e23427a6c6df25580298bc832
Blue Liang <liangkangnan@126.com>
2017-08-17 21:16:31 +0800

    board:stm32_pz6806l: add leds on board definition
    Signed-off-by: Blue Liang <liangkangnan@126.com>

--- boards/arm/stm32_pz6806l/board.h
+++ boards/arm/stm32_pz6806l/board.h
@@ -9,4 +9,31 @@
 
 #include <soc.h>
 
+/* leds on board definition*/
+#define BOARD_LED_NUM   8
+
+#define BOARD_LED1_PIN   0
+#define BOARD_LED1_PORT "GPIOC"
+
+#define BOARD_LED2_PIN   1
+#define BOARD_LED2_PORT "GPIOC"
+
+#define BOARD_LED3_PIN   2
+#define BOARD_LED3_PORT "GPIOC"
+
+#define BOARD_LED4_PIN   3
+#define BOARD_LED4_PORT "GPIOC"
+
+#define BOARD_LED5_PIN   4
+#define BOARD_LED5_PORT "GPIOC"
+
+#define BOARD_LED6_PIN   5
+#define BOARD_LED6_PORT "GPIOC"
+
+#define BOARD_LED7_PIN   6
+#define BOARD_LED7_PORT "GPIOC"
+
+#define BOARD_LED8_PIN   7
+#define BOARD_LED8_PORT "GPIOC"
+
 #endif /* __INC_BOARD_H */
